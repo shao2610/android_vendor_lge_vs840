54e98f9cf6f752ca881797b98a80c24c8076616d
shao2610 <1358410784@qq.com>
2014-06-13 16:26:09 +0800

    Updated blobs for 10.2

--- vs840-vendor-blobs.mk
+++ vs840-vendor-blobs.mk
@@ -106,10 +106,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	vendor/lge/vs840/proprietary/lib/libaudioalsa.so:system/lib/libaudioalsa.so
 
-# Camera
-PRODUCT_COPY_FILES += \
-	vendor/lge/vs840/proprietary/lib/libcameraservice.so:system/lib/libcameraservice.so
-
 # Misc services
 PRODUCT_COPY_FILES += \
 	vendor/lge/vs840/proprietary/bin/ATFWD-daemon:system/bin/ATFWD-daemon \
